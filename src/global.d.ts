581eaa25bd8a9b6cd028c154f241c98de246aa7e
Deren Ozen <deren.ozen@student.kdg.be>
2025-12-28 22:07:08 +0100

    reversing environment variables changes

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,15 +7,3 @@ declare module '*.css' {
     const content: { [className: string]: string };
     export default content;
 }
-
-interface ImportMetaEnv {
-    readonly VITE_API_URL?: string;
-    readonly VITE_AI_URL?: string;
-    readonly VITE_KEYCLOAK_URL?: string;
-    readonly VITE_KEYCLOAK_REALM?: string;
-    readonly VITE_KEYCLOAK_CLIENT_ID?: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
